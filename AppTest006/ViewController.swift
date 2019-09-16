e70c5fed3aaac02c230b621c9370e281ee1f0b29
mingoo <kmui456@gmail.com>
2019-09-16 15:42:08 +0900

    Initial Commit

---
+++ AppTest006/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTest006
+//
+//  Created by min goo on 16/09/2019.
+//  Copyright © 2019 min goo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
